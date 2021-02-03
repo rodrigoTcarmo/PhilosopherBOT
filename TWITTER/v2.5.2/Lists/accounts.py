5d8f67ae57754aedf9229067bfc42006b48752cd
Rodrigo Carmo <49178471+rodrigoTcarmo@users.noreply.github.com>
2021-02-03 16:21:03 -0300

    Update PhilosopherBOT

---
+++ TWITTER/v2.5.2/Lists/accounts.py
@@ -0,0 +1,17 @@
+"""
+posting.py
+Philosopher Bot
+---------------
+Created by Caio Madeira (@sudomaidera)
+Co-worker: Rodrigo Carmo @rodrigoblock
+
+instagram: @sudomadeira
+Twitter: @bot_philospher
+Avaliable on Discord too!
+
+"""
+contas_list = [
+
+    "PARA PRESERVAR AS PESSOAS, NAO PUBLICAMOS AS CONTAS"
+
+]
---
+++ TWITTER/v2.5.2/README.md
@@ -0,0 +1,2 @@
+
+A versão depois dessa trata-se da 2.5.3. Foi necessário um rollback
